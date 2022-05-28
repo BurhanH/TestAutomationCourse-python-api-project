cce1638798c1ae2dc04a5a7c8bdc2ccb5dfbd29e
Svetlana Kargina <lanaka@yandex.ru>
2022-05-28 14:37:01 +0300

    more tests for characters

--- tests/test_get_characters.py
+++ tests/test_get_characters.py
@@ -53,7 +53,57 @@ class TestCharacters(unittest.TestCase):
         body2 = response.json()
         self.assertNotEqual(body1, body2)
 
-    #cheking character attributes
+    def test_character_by_random(self):
+        response = requests.get(BASE_URL + "character/random")
+        self.assertEqual(response.status_code, 200)
+        body = response.json()
+        self.assertTrue(len(body) == 1)
+
+    def test_character_limit_offset(self):
+        offset = 0
+        step = 50
+        count = 10
+        while count > 0:
+            response = requests.get(BASE_URL + f"characters?limit={step}&offset={offset}")
+            self.assertEqual(response.status_code, 200)
+            body = response.json()
+            offset += step
+            count = len(body)
+            self.assertTrue(len(body) <= step)
+
+    def test_character_invalid_limit(self):
+        step = -10
+        response = requests.get(BASE_URL + f"characters?limit={step}")
+        self.assertEqual(response.status_code, 200)
+        body = response.json()
+        self.assertTrue(len(body) == 0)
+
+    def test_character_by_name(self):
+        response = requests.get(BASE_URL + "characters/?name=Walter+White")
+        self.assertEqual(response.status_code, 200)
+        body = response.json()
+        self.assertTrue(len(body) == 1)
+        self._check_character(body[0])
+        self.assertEqual(body[0]["name"], "Walter White")
+
+    def test_character_by_invalid_name(self):
+        name = "unknown"
+        response = requests.get(BASE_URL + f"characters/?name={name}")
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
+        body = response.json()
+        self.assertTrue(len(body) == 0)
+
+    def test_character_by_part_name(self):
+        response = requests.get(BASE_URL + "characters/?name=Walter")
+        self.assertEqual(response.status_code, 200)
+        body = response.json()
+        self.assertTrue(len(body) > 0)
+        for character in body:
+            self._check_character(character)
+            self.assertTrue("Walter" in character["name"])
+
+    #checking character attributes
     def _check_character(self, character):
         self.assertTrue("char_id" in character)
         self.assertTrue("name" in character)
