7ff86e3298f79801b55d7527154a14da5db54d02
Svetlana Kargina <lanaka@yandex.ru>
2022-06-11 22:33:09 +0300

    Create common method to fetch for RESTFull API

--- tests/test_get_characters.py
+++ tests/test_get_characters.py
@@ -1,62 +1,46 @@
-import unittest
-import requests
-BASE_URL = "https://www.breakingbadapi.com/api/"
+from rest_api_test_case_base import RestApiTestCaseBase
 
 
-class TestCharacters(unittest.TestCase):
+class TestCharacters(RestApiTestCaseBase):
 
     def test_all_characters(self):
-        response = requests.get(BASE_URL + "characters")
-        self.assertEqual(response.status_code, 200)
-        body = response.json()
+        body = self.fetch_json("characters")
         self.assertTrue(len(body) > 0)
         for character in body:
             self._check_character(character)
 
     def test_single_character(self):
-        response = requests.get(BASE_URL + "characters/1")
-        self.assertEqual(response.status_code, 200)
-        body = response.json()
+        body = self.fetch_json("characters/1")
         self.assertEqual(len(body), 1)
         self.assertEqual(body[0]["char_id"], 1)
         self._check_character(body[0])
 
     def test_single_character_invalid_id(self):
-        response = requests.get(BASE_URL + "characters/invalid_id")
-        self.assertNotEqual(response.status_code, 200)
+        response = self.fetch("characters/invalid_id")
+        self.assertNotEqual(response.status_code, 200, 
+                            "Request with an invalid_id should not be successful")
 
     def test_single_character_not_existing(self):
-        response = requests.get(BASE_URL + "characters")
-        body = response.json()
+        body = self.fetch_json("characters")
         num = max([i["char_id"] for i in body])
-        response = requests.get(BASE_URL + "characters/" + str(num + 1))
-        self.assertEqual(response.status_code, 200)
-        body = response.json()
+        body = self.fetch_json("characters/" + str(num + 1))
         self.assertEqual(len(body), 0)
 
     def test_character_by_category(self):
-        response = requests.get(BASE_URL + "characters/?category=Better+Call+Saul")
-        self.assertEqual(response.status_code, 200)
-        body = response.json()
+        body = self.fetch_json("characters/?category=Better+Call+Saul")
         self.assertTrue(len(body) > 0)
         for character in body:
             self._check_character(character)
             self.assertTrue("Better Call Saul" in character["category"])
     
     def test_character_by_category_limit(self):
-        response = requests.get(BASE_URL + "characters/?category=Better+Call+Saul&limit=2")
-        self.assertEqual(response.status_code, 200)
-        body1 = response.json()
+        body1 = self.fetch_json("characters/?category=Better+Call+Saul&limit=2")
         self.assertTrue(len(body1) <= 2)
-        response = requests.get(BASE_URL + "characters/?category=Better+Call+Saul&limit=2&offset=2")
-        self.assertEqual(response.status_code, 200)
-        body2 = response.json()
+        body2 = self.fetch_json("characters/?category=Better+Call+Saul&limit=2&offset=2")
         self.assertNotEqual(body1, body2)
 
     def test_character_by_random(self):
-        response = requests.get(BASE_URL + "character/random")
-        self.assertEqual(response.status_code, 200)
-        body = response.json()
+        body = self.fetch_json("character/random")
         self.assertTrue(len(body) == 1)
 
     def test_character_limit_offset(self):
@@ -64,40 +48,29 @@ class TestCharacters(unittest.TestCase):
         step = 50
         count = 10
         while count > 0:
-            response = requests.get(BASE_URL + f"characters?limit={step}&offset={offset}")
-            self.assertEqual(response.status_code, 200)
-            body = response.json()
+            body = self.fetch_json(f"characters?limit={step}&offset={offset}")
             offset += step
             count = len(body)
             self.assertTrue(len(body) <= step)
 
     def test_character_invalid_limit(self):
         step = -10
-        response = requests.get(BASE_URL + f"characters?limit={step}")
-        self.assertEqual(response.status_code, 200)
-        body = response.json()
+        body = self.fetch_json(f"characters?limit={step}")
         self.assertTrue(len(body) == 0)
 
     def test_character_by_name(self):
-        response = requests.get(BASE_URL + "characters/?name=Walter+White")
-        self.assertEqual(response.status_code, 200)
-        body = response.json()
+        body = self.fetch_json("characters/?name=Walter+White")
         self.assertTrue(len(body) == 1)
         self._check_character(body[0])
         self.assertEqual(body[0]["name"], "Walter White")
 
     def test_character_by_invalid_name(self):
         name = "unknown"
-        response = requests.get(BASE_URL + f"characters/?name={name}")
-        print(response.json())
-        self.assertEqual(response.status_code, 200)
-        body = response.json()
+        body = self.fetch_json(f"characters/?name={name}")
         self.assertTrue(len(body) == 0)
 
     def test_character_by_part_name(self):
-        response = requests.get(BASE_URL + "characters/?name=Walter")
-        self.assertEqual(response.status_code, 200)
-        body = response.json()
+        body = self.fetch_json("characters/?name=Walter")
         self.assertTrue(len(body) > 0)
         for character in body:
             self._check_character(character)
