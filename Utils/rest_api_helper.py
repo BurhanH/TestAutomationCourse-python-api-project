b89a5c1649b7ae3efc2db1eac6ff1fda8ae79ada
Baur Urazalinov <burazalinov@signifyhealth.com>
2022-06-20 11:22:32 -0500

    Corrected
    Corrected
    
    Corrected

--- Utils/rest_api_helper.py
+++ Utils/rest_api_helper.py
@@ -1,15 +1,12 @@
 import requests
 
 
-class RestApiHelper:
-    def __init__(self, base_url):
-        self.base_url = base_url
+def fetch_json(path):
+    response = requests.get(path)
+    if response.status_code != 200:
+        raise RuntimeError(f"Invalid result: {response.status_code}")
+    return response.json()
 
-    def fetch_json(self, path):
-        response = requests.get(self.base_url + path)
-        if response.status_code != 200:
-            raise RuntimeError(f"Invalid result: {response.status_code}")
-        return response.json()
-    
-    def fetch(self, path):
-        return requests.get(self.base_url + path)
+
+def fetch(path):
+    return requests.get(path)
--- tests/test_get_characters.py
+++ tests/test_get_characters.py
@@ -1,51 +1,48 @@
 import unittest
-from utils.rest_api_helper import RestApiHelper as RestAPI
+from utils.rest_api_helper import fetch, fetch_json
 BASE_URL = "https://www.breakingbadapi.com/api/"
 
 
 class TestCharacters(unittest.TestCase):
-    def __init__(self, method_name: str = ...) -> None:
-        super().__init__(method_name)
-        self.api = RestAPI(BASE_URL)
 
     def test_all_characters(self):
-        body = self.api.fetch_json("characters")
+        body = fetch_json(f'{BASE_URL}characters')
         self.assertTrue(len(body) > 0)
         for character in body:
             self._check_character(character)
 
     def test_single_character(self):
-        body = self.api.fetch_json("characters/1")
+        body = fetch_json(f'{BASE_URL}characters/1')
         self.assertEqual(len(body), 1)
         self.assertEqual(body[0]["char_id"], 1)
         self._check_character(body[0])
 
     def test_single_character_invalid_id(self):
-        response = self.api.fetch("characters/invalid_id")
+        response = fetch(f'{BASE_URL}characters/invalid_id')
         self.assertNotEqual(response.status_code, 200, 
                             "Request with an invalid_id should not be successful")
 
     def test_single_character_not_existing(self):
-        body = self.api.fetch_json("characters")
+        body = fetch_json(f'{BASE_URL}characters')
         num = max([i["char_id"] for i in body])
-        body = self.api.fetch_json("characters/" + str(num + 1))
+        body = fetch_json(f'{BASE_URL}characters/{str(num + 1)}')
         self.assertEqual(len(body), 0)
 
     def test_character_by_category(self):
-        body = self.api.fetch_json("characters/?category=Better+Call+Saul")
+        body = fetch_json(f'{BASE_URL}characters/?category=Better+Call+Saul')
         self.assertTrue(len(body) > 0)
         for character in body:
             self._check_character(character)
             self.assertTrue("Better Call Saul" in character["category"])
     
     def test_character_by_category_limit(self):
-        body1 = self.api.fetch_json("characters/?category=Better+Call+Saul&limit=2")
+        body1 = fetch_json(f'{BASE_URL}characters/?category=Better+Call+Saul&limit=2')
         self.assertTrue(len(body1) <= 2)
-        body2 = self.api.fetch_json("characters/?category=Better+Call+Saul&limit=2&offset=2")
+        body2 = fetch_json(f'{BASE_URL}characters/?category=Better+Call+Saul&limit=2&offset=2')
         self.assertNotEqual(body1, body2)
 
     def test_character_by_random(self):
-        body = self.api.fetch_json("character/random")
+        body = fetch_json(f'{BASE_URL}character/random')
         self.assertTrue(len(body) == 1)
 
     def test_character_limit_offset(self):
@@ -53,29 +50,29 @@ class TestCharacters(unittest.TestCase):
         step = 50
         count = 10
         while count > 0:
-            body = self.api.fetch_json(f"characters?limit={step}&offset={offset}")
+            body = fetch_json(f'{BASE_URL}characters?limit={step}&offset={offset}')
             offset += step
             count = len(body)
             self.assertTrue(len(body) <= step)
 
     def test_character_invalid_limit(self):
         step = -10
-        body = self.api.fetch_json(f"characters?limit={step}")
+        body = fetch_json(f'{BASE_URL}characters?limit={step}')
         self.assertTrue(len(body) == 0)
 
     def test_character_by_name(self):
-        body = self.api.fetch_json("characters/?name=Walter+White")
+        body = fetch_json(f'{BASE_URL}characters/?name=Walter+White')
         self.assertTrue(len(body) == 1)
         self._check_character(body[0])
         self.assertEqual(body[0]["name"], "Walter White")
 
     def test_character_by_invalid_name(self):
         name = "unknown"
-        body = self.api.fetch_json(f"characters/?name={name}")
+        body = fetch_json(f'{BASE_URL}characters/?name={name}')
         self.assertTrue(len(body) == 0)
 
     def test_character_by_part_name(self):
-        body = self.api.fetch_json("characters/?name=Walter")
+        body = fetch_json(f'{BASE_URL}characters/?name=Walter')
         self.assertTrue(len(body) > 0)
         for character in body:
             self._check_character(character)
