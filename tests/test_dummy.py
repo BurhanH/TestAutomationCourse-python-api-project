fa86a42683b3844516a8db30e353ae1d4632fe75
Baur Urazalinov <burazalinov@signifyhealth.com>
2022-06-20 10:06:52 -0500

    Updated pytest.ini Updated test_dummy.py file with samples.

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,11 +1,68 @@
 import unittest
+import pytest
 
 
-class TestDummy(unittest.TestCase):
-            
-    def test_equal_should_pass(self):
-        self.assertEqual(1, 1)
+class TestSuite(unittest.TestCase):
+    def setUp(self) -> None:
+        # Do something
+        pass
 
+    @pytest.mark.smoke
+    def test_1(self):
+        self.assertTrue(1, 1)
+
+    @pytest.mark.smoke
+    def test_2(self):
+        self.assertTrue(1, 1)
+
+    @pytest.mark.regression
+    def test_3(self):
+        self.assertTrue(1, 1)
+
+    @pytest.mark.regression
+    def test_4(self):
+        self.assertTrue(1, 1)
+
+    @pytest.mark.regression
+    def test_5(self):
+        self.assertTrue(1, 1)
+
+    def tearDown(self) -> None:
+        # Do something
+        pass
+
+
+class TestSuite2(unittest.TestCase):
+    def setUp(self) -> None:
+        # Do something
+        pass
+
+    @pytest.mark.regression
+    def test_11(self):
+        self.assertTrue(1, 1)
+
+    @pytest.mark.regression
+    def test_12(self):
+        self.assertTrue(1, 1)
+
+    @pytest.mark.skip
+    def test_13(self):
+        # Under development
+        pass
+
+    @pytest.mark.skip
+    def test_14(self):
+        # Under development
+        pass
+
+    @pytest.mark.skip
+    def test_15(self):
+        # Under development
+        pass
+
+    def tearDown(self) -> None:
+        # Do something
+        pass
 
 
 if __name__ == '__main__':
